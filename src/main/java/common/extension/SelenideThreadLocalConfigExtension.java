a641b4df692c1ef0fdbd054c4cf619e760ecc955
Victor Ivanov <vivanov@allot.com>
2026-05-19 20:56:03 +0300

    switch off headless mode to ee if we gonna have screenshots in allure

--- src/main/java/common/extension/SelenideThreadLocalConfigExtension.java
+++ src/main/java/common/extension/SelenideThreadLocalConfigExtension.java
@@ -2,12 +2,16 @@ package common.extension;
 
 import api.config.Config;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import io.qameta.allure.selenide.LogType;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.logging.Level;
 
 public class SelenideThreadLocalConfigExtension implements BeforeEachCallback {
 
@@ -18,7 +22,8 @@ public class SelenideThreadLocalConfigExtension implements BeforeEachCallback {
         Configuration.baseUrl = Config.getProperty("base.ui.url");
         Configuration.browserSize = Config.getProperty("browser.size");
         Configuration.browser = Config.getProperty("browser");
-        /*SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
+        Configuration.browserCapabilities.setCapability("goog:loggingPrefs", Map.of("browser", "ALL"));
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
                 .screenshots(true)
                 .savePageSource(true)
                 .includeSelenideSteps(true)
@@ -26,7 +31,7 @@ public class SelenideThreadLocalConfigExtension implements BeforeEachCallback {
                 .enableLogs(LogType.PERFORMANCE, Level.INFO)
                 .enableLogs(LogType.SERVER, Level.INFO)
                 .enableLogs(LogType.CLIENT, Level.INFO)
-                .enableLogs(LogType.DRIVER, Level.INFO));*/
+                .enableLogs(LogType.DRIVER, Level.INFO));
 
         ChromeOptions chromeOptions = getChromeOptions();
         Map<String, Object> selenoidOptions = new HashMap<>();
--- src/test/java/ui/BaseUiTest.java
+++ src/test/java/ui/BaseUiTest.java
@@ -2,28 +2,20 @@ package ui;
 
 import api.ApiBaseTest;
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
 import common.extension.AdminSessionExtension;
 import common.extension.BrowserMatchExtension;
+import common.extension.ScreenshotOnFailureExtension;
 import common.extension.SelenideThreadLocalConfigExtension;
 import common.extension.UserSessionExtension;
-import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith({SelenideThreadLocalConfigExtension.class,
+        ScreenshotOnFailureExtension.class,
         AdminSessionExtension.class,
         UserSessionExtension.class,
         BrowserMatchExtension.class})
 public class BaseUiTest extends ApiBaseTest {
-
-    @BeforeAll
-    public static void setupAllureScreenshots() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide()
-                .screenshots(true)
-                .savePageSource(true));
-    }
 
     @AfterEach
     public void tearDown() {
