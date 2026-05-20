41975bcf577b5a2f156d03a377964ef4c2ebeab2
Victor Ivanov <vivanov@allot.com>
2026-05-20 20:24:44 +0300

    switch off headless mode to ee if we gonna have screenshots in allure

--- src/main/java/api/specs/RequestSpecs.java
+++ src/main/java/api/specs/RequestSpecs.java
@@ -4,7 +4,7 @@ import api.config.Config;
 import api.models.LoginUserRequest;
 import api.requests.skeleton.Endpoint;
 import api.requests.skeleton.requests.CrudRequester;
-import com.github.viclovsky.swagger.coverage.SwaggerCoverageRestAssured;
+import com.github.viclovsky.swagger.coverage.SwaggerCoverageV3RestAssured;
 import io.qameta.allure.restassured.AllureRestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
@@ -13,14 +13,10 @@ import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class RequestSpecs {
 
-    /*private static Map<String, String> authHeaderMap = new HashMap<>(
-            Map.of("admin", "Basic YWRtaW46YWRtaW4=")
-    );*/
     private static final ThreadLocal<Map<String, String>> authHeaderMap =
             ThreadLocal.withInitial(() -> new HashMap<>(Map.of("admin", "Basic YWRtaW46YWRtaW4=")));
 
@@ -34,8 +30,10 @@ public class RequestSpecs {
         return new RequestSpecBuilder()
                 .setContentType(ContentType.JSON)
                 .setAccept(ContentType.JSON)
-                .addFilters(List.of(new RequestLoggingFilter(), new ResponseLoggingFilter(),
-                        new AllureRestAssured(), new SwaggerCoverageRestAssured()))
+                .addFilter(new RequestLoggingFilter())
+                .addFilter(new ResponseLoggingFilter())
+                .addFilter(new AllureRestAssured())
+                .addFilter(new SwaggerCoverageV3RestAssured())
                 .setBaseUri(Config.getProperty("base.api.url") + Config.getProperty("api.version"));
     }
 
